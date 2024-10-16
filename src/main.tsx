4ff3e38671eb2f1afd876ffeda4c0f787733c325
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-16 10:11:26 -0400

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.tsx'
+import { Error } from './pages/Error/Error.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+// import s from "./style.module.scss";
+
+export function Error() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <p onClick={() => navigate("/")}>Retourner sur la page d'accueil</p>
+    </div>
+  );
+}
