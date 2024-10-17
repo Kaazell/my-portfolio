33180d0ab175dd00d17748082c6e404a3d6ab110
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-17 10:05:06 -0400

    feat: React Particles background effect

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,9 @@
+import s from "./style.module.scss";
+
 export function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className={s.footer}>
+      <p className={s.footer__text}>Footer text</p>
+    </footer>
+  );
 }
