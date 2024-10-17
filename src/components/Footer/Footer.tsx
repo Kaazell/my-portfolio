6ff85e64ea9d6560d4e949968f05fde935929f79
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-17 10:13:19 -0400

    feat(mobile): Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,10 @@ import s from "./style.module.scss";
 export function Footer() {
   return (
     <footer className={s.footer}>
-      <p className={s.footer__text}>Footer text</p>
+      <p className={s.footer__text}>Atef Lachhab</p>
+      <p className={s.footer__text}>
+        Copyright © <span id="current-year">2024</span>
+      </p>
     </footer>
   );
 }
