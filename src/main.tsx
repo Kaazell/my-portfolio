14dc34604c47c4cf7b02b34bdf2711c4d0a1d2d4
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-18 06:12:46 -0400

    fix: burger menu overlay/wrap z-index and positioning fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App.tsx'
-import { Error } from './pages/Error/Error.tsx'
-import './index.css'
+import App from "./App.tsx";
+import { Error } from "./pages/Error/Error.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import Home from "./pages/Home/Home.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}></Route>
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
+);
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./style.module.scss";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div className={s.welcome}>Développeur FullStack</div>
+    </>
+  );
+};
+
+export default Home;
