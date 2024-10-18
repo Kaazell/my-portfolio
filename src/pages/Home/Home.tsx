8906dd0055746dfc779ef210dfe96b1c765ea7a3
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-18 09:59:00 -0400

    feat: Button component hover transition

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import s from "./style.module.scss";
+import Button from "../../components/Button/Button";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className={s.welcome}>Développeur FullStack</div>
+      <div className={s.landing}>
+        <h1>Développeur FullStack</h1>
+        <h2>
+          J'utilise principlaement React et NodeJS pour concevoir des solutions
+          modernes et efficaces
+        </h2>
+        <Button buttonName="Prendre RDV >" />
+      </div>
     </>
   );
 };
