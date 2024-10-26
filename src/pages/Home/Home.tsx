aef53e8b9a8d2db39d12377553cc129e08c5cc30
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-26 10:19:20 -0400

    feat: form

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -23,6 +23,25 @@ const Home: React.FC = () => {
         <h3>&lt;Projets /&gt;</h3>
         <WorkCards />
       </section>
+      <section className={s.form_container}>
+        <h3>&lt;Contact /&gt;</h3>
+        <form
+          className={s.contact_form}
+          action="https://formsubmit.co/atef.lachhab1@gmail.com"
+          method="POST"
+        >
+          <input type="text" name="name" placeholder="Votre nom" required />
+          <input type="email" name="email" placeholder="Votre email" required />
+          <textarea
+            name="message"
+            placeholder="Votre message"
+            rows={10}
+            cols={60}
+            required
+          ></textarea>
+          <button type="submit">Envoyer</button>
+        </form>
+      </section>
     </>
   );
 };
