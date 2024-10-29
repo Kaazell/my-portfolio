dc7fc8a1c449f55ccdb2b1260003a698e525e12f
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-29 17:39:08 -0400

    feat: Error page desktop

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -6,11 +6,8 @@ export function Error() {
   return (
     <div className={s.container}>
       <h1>404</h1>
-      <h2>Oups! Cette page n'existe pas.</h2>
-      <Button
-        destination="#home_section"
-        buttonName="Continuer l'exploration >"
-      />
+      <p>Oups! Cette page n'existe pas.</p>
+      <Button destination="/" buttonName="Explorer >" />
     </div>
   );
 }
