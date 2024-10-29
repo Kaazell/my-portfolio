5321fe93302c9e2914a6302d8d3e63efe987dd34
Atef Lachhab <atef.lachhab1@gmail.com>
2024-10-29 13:05:12 -0400

    feat: navigation links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Burger from "../Burger/Burger";
 import s from "./style.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,7 +11,9 @@ import {
 
 export function Header() {
   const [showBurger, setShowBurger] = useState(window.innerWidth <= 850);
+  const navigate = useNavigate();
 
+  // Gestion du changement de taille d'écran
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 850px)");
     const handleMediaChange = () => setShowBurger(mediaQuery.matches);
@@ -19,23 +22,64 @@ export function Header() {
     return () => mediaQuery.removeEventListener("change", handleMediaChange);
   }, []);
 
+  // Fonction de gestion des clics pour la navigation
+  const handleLinkClick = (sectionId: string) => {
+    // Redirige vers la page d'accueil
+    navigate("/");
+
+    // Scroll vers la section voulue après un léger délai
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100);
+  };
+
   return (
     <header id="header">
       <div className={s.header}>
-        <h1 className={s.title}>&lt;ATEF LACHHAB /&gt;</h1>
+        <h1 className={s.title}>
+          <a
+            href="/#home_section"
+            onClick={() => handleLinkClick("home_section")}
+          >
+            &lt;ATEF LACHHAB /&gt;
+          </a>
+        </h1>
         <nav>
           <ul>
             <li>
-              <a href="#home_section">Accueil</a>
+              <a
+                href="#home_section"
+                onClick={() => handleLinkClick("home_section")}
+              >
+                Accueil
+              </a>
             </li>
             <li>
-              <a href="#skills_section">Compétences</a>
+              <a
+                href="#skills_section"
+                onClick={() => handleLinkClick("skills_section")}
+              >
+                Compétences
+              </a>
             </li>
             <li>
-              <a href="#portfolio_section">Projets</a>
+              <a
+                href="#portfolio_section"
+                onClick={() => handleLinkClick("portfolio_section")}
+              >
+                Projets
+              </a>
             </li>
             <li>
-              <a href="#contact_form">Contact</a>
+              <a
+                href="#contact_form"
+                onClick={() => handleLinkClick("contact_form")}
+              >
+                Contact
+              </a>
             </li>
             <li className={s.first_sm_icon}>
               <a
