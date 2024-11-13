c1ba02d98800097803b521f3e0b3c552388a7ec8
Atef Lachhab <atef.lachhab1@gmail.com>
2024-11-13 15:53:21 -0500

    feat: refactoring error page style

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -5,8 +5,8 @@ import Button from "../../components/Button/Button";
 export function Error() {
   return (
     <div className={s.container}>
-      <h1>404</h1>
-      <p>Oups! Cette page n'existe pas.</p>
+      <h1 className={s.container__title}>404</h1>
+      <p className={s.container__text}>Oups! Cette page n'existe pas.</p>
       <Button destination="/" buttonName="Explorer >" />
     </div>
   );
